d82259e54df9241c340a806d4313bd59a1dfd932
Victor Bashorun <vbashorun@gmail.com>
2016-05-22 13:24:48 -0500

    built out site-loading mechanism and animations

---
+++ js/marksman.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function() {
+    
+    $(window).load(function() {
+        
+        setTimeout(function() {
+            $("#loadBackdrop").css({"opacity":"0", "height":"0" });
+        }, 1000);
+       
+    });
+});
