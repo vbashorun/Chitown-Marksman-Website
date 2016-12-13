6609707523cdb7a1279a7a7118e694f6201f7132
Victor  Bashorun <vbashorun@gmail.com>
2016-12-12 21:00:22 -0800

    created splash page. Added some commented code for a slide show later

--- app/app.js
+++ app/app.js
@@ -3,17 +3,21 @@
 var app = angular.module('ChitownMarksman', ['ngRoute']);
 
 app.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+  //$locationProvider.hashPrefix('!');
 
   $routeProvider
     .when('/', {
         controller: 'HomeController',
         templateUrl: 'views/home.html'
     })
-    .when('/suggestion/:id', {
+    .when('/portfolio', {
+        controller: 'PortfolioController',
+        templateUrl: 'views/portfolio.html'
+    })
+    /*.when('/suggestion/:id', {
         controller: 'SuggestionController',
         templateUrl: 'views/suggestion.html'
-    })
+    })*/
     .otherwise({
         redirectTo: '/'
     });
--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,21 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
     sourcemaps = require('gulp-sourcemaps'),
-    rename = require("gulp-rename");
+    rename = require('gulp-rename'),
+    shell = require('gulp-shell');
+
+// Views
+gulp.task('view-refresh', function () {
+    gulp.src('./app/views/*.html')
+    .pipe(livereload());
+});
+
+gulp.task('view-watch', function () {
+    livereload.listen();
+    gulp.watch('./app/views/*.html', ['view-refresh']);
+});
  
+// CSS
 gulp.task('sass', function () {
     gulp.src('./app/styles/*.scss')
     .pipe(sass())
@@ -14,9 +27,25 @@ gulp.task('sass', function () {
 });
  
 gulp.task('sass-watch', function () {
-    livereload.listen({ host: 'localhost', port: 8000 });
+    livereload.listen();
     gulp.watch('./app/styles/*.scss', ['sass']);
 });
+
+// Javascript
+gulp.task('js-refresh', function () {
+    gulp.src('./app/controllers/*.js')
+    .pipe(livereload());
+});
+
+gulp.task('controller-watch', function () {
+    livereload.listen();
+    gulp.watch('./app/controllers/*.js', ['js-refresh']);
+});
+
+// CMD
+gulp.task('startServer', shell.task([
+  'npm start'
+]));
 
 // browser refresh -- 
 // serves as an example of triggering livereload without any tasks in between
@@ -30,6 +59,6 @@ gulp.task('test-watch', function() {
     gulp.watch('./app/index.html', ['testResponse']);
 });*/
 
-gulp.task('default', function() {
+gulp.task('default', ['view-watch','controller-watch', 'sass-watch', 'startServer'], function() {
   // place code for your default task here
 });
