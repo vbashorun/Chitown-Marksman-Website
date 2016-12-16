aca3e5adcecef792267b87b36f11a77e9ffc647a
Victor  Bashorun <vbashorun@gmail.com>
2016-12-16 06:31:39 -0800

    HAD successfully implemented masonryJS but for some reason .load function is not firing in chrome or firefox. Looking into issue but saving code and moving on since it did work at some point.

--- app/controllers/PortfolioController.js
+++ app/controllers/PortfolioController.js
@@ -1,9 +1,42 @@
 app.controller('PortfolioController', ['$scope', function($scope) {
-       
+    
+    var tabletWidth = 768;  // minimum width before masonryJS is instantiated
+    //var masonryData = $('.grid').data('masonry'); //used for testing presence of masonry
+    var masonryParams = {
+        itemSelector: '.gallery-card',
+        columnWidth: '.grid-sizer',
+        percentPosition: true,
+        gutter: 0
+    };
+    
     $(document).ready(function() {
         //alert("portfolio controller initialized");
         
+        /*$(window).load(function() {
+            alert("window loaded");
+            
+            var browserWindow = $(this);
+            
+            if (browserWindow.width() >= tabletWidth)
+            {
+                $('.grid').masonry(masonryParams);
+            }
+            
+            // adjust masonryJS on resize
+            browserWindow.resize(function() {
+                if (browserWindow.width() < tabletWidth)
+                {
+                    $('.grid').masonry('destroy');
+                }
+                else
+                    $('.grid').masonry(masonryParams);
+            });
+        });*/
         
+        //alert("bypassed window load");
     });
     
+    $(window).load(function() { 
+        alert("window loaded outside of ready"); 
+    });
 }]);
