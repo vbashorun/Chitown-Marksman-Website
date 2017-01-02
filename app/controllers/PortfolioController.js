b53fd2c1520ecfdc6cfb59d3f142fef7b96cb815
Victor  Bashorun <vbashorun@gmail.com>
2017-01-01 22:40:26 -0800

    added loading animation to portfolio view and adjusted logic for rendering content.

--- app/controllers/PortfolioController.js
+++ app/controllers/PortfolioController.js
@@ -1,8 +1,8 @@
 app.controller('PortfolioController', ['$scope', '$http', function($scope, $http) {
     
     var tabletWidth = 768;  // minimum width before masonryJS is instantiated
-    //var masonryData = $('.grid').data('masonry'); //used for testing presence of masonry
-    //alert("masonry: " + masonryData);
+    // var masonryData = $('.grid').data('masonry'); //used for testing presence of masonry and accessing properties
+    // console.log("masonry: " + masonryData);
     var masonryParams = {
         itemSelector: '.gallery-card',
         columnWidth: '.portfolio-grid-sizer',
@@ -12,36 +12,9 @@ app.controller('PortfolioController', ['$scope', '$http', function($scope, $http
     
     $(document).ready(function() {
         
-        // following value persists if data doesn't load properly
-        $scope.cards = [
-            {
-                name: 'no galleries available at this time'
-            }
-        ];
-        
+        $scope.cards = [];
         $scope.selectedCategory = "featured";
-        
         updatePortfolioView($scope.selectedCategory);
-        
-        $('.grid').imagesLoaded().always( function( instance ) {
-            
-            var browserWindow = $(window);
-            
-            if (browserWindow.width() >= tabletWidth)
-            {
-                $('.grid').masonry(masonryParams);
-            }
-            
-            // adjust masonryJS on resize
-            browserWindow.resize(function() {
-                if (browserWindow.width() < tabletWidth)
-                {
-                    $('.grid').masonry('destroy');
-                }
-                else
-                    $('.grid').masonry(masonryParams);
-            });
-        });
     });
     
     $scope.handleCategory = function(eventObject) {
@@ -50,10 +23,15 @@ app.controller('PortfolioController', ['$scope', '$http', function($scope, $http
         var isSelected = $(element).hasClass("categorySelected");
         
         if (!isSelected) {
-            $('.categorySelected').removeClass("categorySelected");
-            $(element).addClass("categorySelected");
-            $scope.selectedCategory = element.getAttribute("data-category");
-            updatePortfolioView($scope.selectedCategory);
+            $('#galleryContainer').css({"opacity" : "0"});
+            $('#loaderContainer').css({"opacity" : "1"});
+            
+            setTimeout(function() {
+                $('.categorySelected').removeClass("categorySelected");
+                $(element).addClass("categorySelected");
+                $scope.selectedCategory = element.getAttribute("data-category");
+                updatePortfolioView($scope.selectedCategory);
+            }, 1000);
         }
     };
     
@@ -62,21 +40,36 @@ app.controller('PortfolioController', ['$scope', '$http', function($scope, $http
     };
     
     function updatePortfolioView(category) {
-        
-        // failed service attempt. preserving because it SHOULD be fixed at some point
-        /*$scope.cards = getGalleries.cards;
-        alert("value of cards: " + $scope.cards);*/
-        //$scope.cards = fakeCards.cards;
     
         // preferably, this data should come from a service component utilized by this method, 
         // not directlypresent in the controller
         
         $http.get("./scripts/php/portfolioQuery.php?tag=" + category)
-        .then(function (response) 
-          {
+        .then(function (response) {
             //$scope.cards = response.data.records;
             $scope.cards = response.data.records;
-          });
+            
+            $('.grid').imagesLoaded( function() {
+            
+                var browserWindow = $(window);
+
+                if (browserWindow.width() >= tabletWidth)
+                    $('.grid').masonry(masonryParams);
+
+                // adjust masonryJS on resize
+                browserWindow.resize(function() {
+                    if (browserWindow.width() < tabletWidth)
+                        $('.grid').masonry('destroy');
+                    else
+                        $('.grid').masonry(masonryParams);
+                });
+
+                setTimeout(function() {
+                    $('.grid').masonry('layout');
+                    $('#galleryContainer').css({"opacity" : "1"});
+                    $('#loaderContainer').css({"opacity" : "0"});
+                }, 8000);
+            });
+        });
     };
-    
 }]);
