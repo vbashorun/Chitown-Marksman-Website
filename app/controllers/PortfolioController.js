ef3b828638311b5b44bbf0375fc233b85de224fe
Victor  Bashorun <vbashorun@gmail.com>
2017-02-17 20:42:51 -0800

    updated portfolio view and controller to use imageOnLoad directive

--- app/controllers/PortfolioController.js
+++ app/controllers/PortfolioController.js
@@ -1,18 +1,26 @@
 app.controller('PortfolioController', ['$scope', '$http', function($scope, $http) {
     
     var tabletWidth = 768;  // minimum width before masonryJS is instantiated
-    // var masonryData = $('.grid').data('masonry'); //used for testing presence of masonry and accessing properties
-    // console.log("masonry: " + masonryData);
-    var masonryParams = {
-        itemSelector: '.gallery-card',
-        columnWidth: '.portfolio-grid-sizer',
-        percentPosition: true,
-        gutter: 0
-    };
+    var browserWindow = $(window);
     
     $(document).ready(function() {
         
+        $scope.masonryParams = {
+            itemSelector: '.gallery-card',
+            columnWidth: '.portfolio-grid-sizer',
+            percentPosition: true,
+            gutter: 0
+        };
         $scope.cards = [];
+        
+        // adjust masonryJS on resize
+        browserWindow.resize(function() {
+            if (browserWindow.width() < tabletWidth)
+                $('.grid').masonry('destroy');
+            else
+                $('.grid').masonry($scope.masonryParams);
+        });
+        
         $scope.selectedCategory = "featured";
         updatePortfolioView($scope.selectedCategory);
     });
@@ -40,36 +48,20 @@ app.controller('PortfolioController', ['$scope', '$http', function($scope, $http
     };
     
     function updatePortfolioView(category) {
-    
-        // preferably, this data should come from a service component utilized by this method, 
-        // not directlypresent in the controller
         
         $http.get("./scripts/php/portfolioQuery.php?tag=" + category)
         .then(function (response) {
-            //$scope.cards = response.data.records;
             $scope.cards = response.data.records;
             
             $('.grid').imagesLoaded( function() {
-            
-                var browserWindow = $(window);
-
-                if (browserWindow.width() >= tabletWidth)
-                    $('.grid').masonry(masonryParams);
-
-                // adjust masonryJS on resize
-                browserWindow.resize(function() {
-                    if (browserWindow.width() < tabletWidth)
-                        $('.grid').masonry('destroy');
-                    else
-                        $('.grid').masonry(masonryParams);
-                });
-
                 setTimeout(function() {
-                    $('.grid').masonry('layout');
                     $('#galleryContainer').css({"opacity" : "1"});
                     $('#loaderContainer').css({"opacity" : "0"});
-                }, 6000);
+                }, 2000);
             });
-        });
+        })
+        .catch(function(err){
+            console.log("error loading portfolio galleries:...", err);
+        });;
     };
 }]);
